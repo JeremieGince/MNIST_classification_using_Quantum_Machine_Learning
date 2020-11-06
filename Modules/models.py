2e38b410bebca9e758d6fb113f4b762004f4ad51
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-11-06 13:09:38 -0500

    Update models.py

--- Modules/models.py
+++ Modules/models.py
@@ -1,15 +1,73 @@
 import torch
+import pennylane as qml
+
+N_QUBITS = 2
+quantum_device = qml.device("default.qubit", wires=N_QUBITS)
 
 
-class BaseModel:
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
+class BaseModel(torch.nn.Module):
+    def __init__(self, **hp):
+        super().__init__()
+        self.hp = hp
 
-    def set_hp(self, **kwargs):
+    def set_hp(self, **hp):
         raise NotImplementedError()
 
-    def fit(self, X, y):
-        raise NotImplementedError()
+    def fit(self, X, y, **kwargs):
+        pass
 
     def predict(self, X):
         raise NotImplementedError()
+
+    def score(self, X, y):
+        raise NotImplementedError()
+
+
+class HybridModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+
+        self.n_layers = 6
+
+        weight_shapes = {"weights": (self.n_layers, N_QUBITS)}
+
+        self.clayer_1 = torch.nn.Linear(2, 4)
+        self.qlayer_1 = qml.qnn.TorchLayer(self.qnode, weight_shapes)
+        self.qlayer_2 = qml.qnn.TorchLayer(self.qnode, weight_shapes)
+        self.clayer_2 = torch.nn.Linear(4, 2)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.clayer_1(x)
+        x_1, x_2 = torch.split(x, 2, dim=1)
+        x_1 = self.qlayer_1(x_1)
+        x_2 = self.qlayer_2(x_2)
+        x = torch.cat([x_1, x_2], axis=1)
+        x = self.clayer_2(x)
+        return self.softmax(x)
+
+    @qml.qnode(quantum_device)
+    def qnode(self, inputs, weights):
+        qml.templates.AngleEmbedding(inputs, wires=range(N_QUBITS))
+        qml.templates.BasicEntanglerLayers(weights, wires=range(N_QUBITS))
+        return [qml.expval(qml.PauliZ(wires=i)) for i in range(N_QUBITS)]
+
+
+class ClassicalModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.clayer_1 = torch.nn.Linear(2, 4)
+        self.clayer_2 = torch.nn.Linear(2, 2)
+        self.clayer_3 = torch.nn.Linear(2, 2)
+        self.clayer_4 = torch.nn.Linear(4, 2)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.clayer_1(x)
+        x_1, x_2 = torch.split(x, 2, dim=1)
+        x_1 = self.clayer_2(x_1)
+        x_2 = self.clayer_3(x_2)
+        x = torch.cat([x_1, x_2], axis=1)
+        x = self.clayer_4(x)
+        return self.softmax(x)
+
