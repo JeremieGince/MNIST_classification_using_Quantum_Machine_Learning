90eaba5f845975dfb33bb0145ec058edb9da1baf
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-11-08 11:24:58 -0500

    Update layers.py

--- Modules/layers.py
+++ Modules/layers.py
@@ -3,6 +3,8 @@ from pennylane import numpy as np
 import torch
 from pennylane.templates import RandomLayers
 import time
+from scipy.ndimage.filters import generic_filter
+
 
 class QuantumConvolutionLayer(torch.nn.Module):
     def __init__(self, **hp):
@@ -19,6 +21,8 @@ class QuantumConvolutionLayer(torch.nn.Module):
         get_circuit = lambda phi, layer_param: self.circuit(phi, layer_param)
         self.qnode = qml.QNode(get_circuit, dev, interface='torch')
 
+        self.output_shape = None
+
     def circuit(self, phi=None, layer_params=None):
         # Encoding of 4 classical input values
         for j in range(self.nb_qubits):
@@ -31,19 +35,44 @@ class QuantumConvolutionLayer(torch.nn.Module):
         return [qml.expval(qml.PauliZ(j)) for j in range(self.nb_qubits)]
 
     def forward(self, x):
+        if self.output_shape is None:
+            self.output_shape = (
+                x.shape[0],
+                x.shape[1] * self.nb_qubits,
+                x.shape[2] // self.kernel_size[0],
+                x.shape[3] // self.kernel_size[1]
+            )
+        print((*[1 for _ in range(len(x.shape) - len(self.kernel_size))], *self.kernel_size))
+        out1 = generic_filter(x, self.q_filter,
+                              size=(*[1 for _ in range(len(x.shape) - len(self.kernel_size))], *self.kernel_size))
+        print(f"out1.shape: {out1.shape}")
+
         # out = torch.Tensor(np.array(list(map(self.convolve, x)))).float()
-        start = time.time()
-        # print("batch start")
-        out = torch.zeros((x.shape[0],x.shape[1]*self.nb_qubits  ,x.shape[2] // self.kernel_size[0], x.shape[3] // self.kernel_size[1])).to(x.device)
+        output_shape = (x.shape[0], x.shape[1]*self.nb_qubits, x.shape[2] // self.kernel_size[0], x.shape[3] // self.kernel_size[1])
+        out = torch.zeros(output_shape).to(x.device)
         for i in range(x.shape[0]):
             for j in range(x.shape[1]):
-                out[i] = self.convolve(x[i,j])
-        # print(f"Elapse time :  {time.time() - start}")
+                out[i] = self.convolve(x[i, j])
+
+        x_flat = self.flatten_x(x)
         return out
+
+    def flatten_x(self, x):
+        out = torch.zeros(self.output_shape).to(x.device)
+        out[:x.shape[0], :x.shape[1], :x.shape[2], :x.shape[3]] = x
+        return out.flatten()
+
+    def unflatten_x(self, x):
+        pass
+
+    def flatten_q_output(self, q_out):
+        out = torch.zeros(self.output_shape).to(x.device)
+        out[0, 0:2] = q_out
+        return out.flatten()
 
     def convolve(self, x):
         # x = torch.squeeze(x)
-        out = torch.zeros((self.nb_qubits,x.shape[0] // self.kernel_size[0], x.shape[1] // self.kernel_size[1])).to(x.device)
+        out = torch.zeros((self.nb_qubits, x.shape[0] // self.kernel_size[0], x.shape[1] // self.kernel_size[1])).to(x.device)
         for j in range(0, x.shape[0] - 1):
             for k in range(0, x.shape[1] - 1):
                 # Process a squared 2x2 region of the image with a quantum circuit
@@ -53,8 +82,11 @@ class QuantumConvolutionLayer(torch.nn.Module):
                 )
                 # Assign expectation values to different channels of the output pixel (j/2, k/2)
                 for c in range(self.nb_qubits):
-                    out[c,j // self.kernel_size[0], k // self.kernel_size[1]] = q_results[c]
+                    out[c, j // self.kernel_size[0], k // self.kernel_size[1]] = q_results[c]
         return out
+
+    def q_filter(self, phi):
+        return self.qnode(phi, self.weights)
 
 
 class QuantumPseudoLinearLayer(torch.nn.Module):
@@ -81,8 +113,8 @@ if __name__ == '__main__':
     import time
 
     s = time.time()
-    q_conv_layer = QuantumConvolutionLayer(kernel_size=(2, 2), nb_qubits=1)
-    inputs = torch.Tensor(np.ones((32, 8, 8))).float()
+    q_conv_layer = QuantumConvolutionLayer(kernel_size=(2, 2), nb_qubits=2)
+    inputs = torch.Tensor(np.ones((32, 1, 8, 8))).float()
     outputs = q_conv_layer(inputs)
     print(outputs.shape)
 
